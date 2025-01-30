b5d18d1950c562ba90bc85803e35117b7316442f
Exxo05 <mrosarioperezcejuela@gmail.com>
2025-01-30 19:01:36 +0100

    añadi el primer contenido de la WeatherAPI

--- script.js
+++ script.js
@@ -6,4 +6,26 @@ window.addEventListener("load", () => {
     bodyElem.style.backgroundImage = `url(IMAGENES/bg${numeroRandom}.jpng)`;
 });
 
+let putCiudad = document.querySelector("get-city");
+putCiudad.addEventListener("keypress", (event) => {
+    if (event.key == "Enter") {
+        fetchDataFromApi();
+    }
+});
 
+let datosAPI = {
+    url: "http://api.openweathermap.org/data/2.5/forecast?q=",
+    key: "7ea15b5ede189da436e9374f884ca5b7",
+};
+
+putCiudad.value = "Madrid";
+fetchDataFromApi();
+putCiudad.value = "";
+
+function fetchDataFromApi() {
+    let ciudadSelecionada = putCiudad.value;
+    fetch((`${datosAPI.url}${ciudadSelecionada}&&appid=${datosAPI.key}`))
+    .then((res) => res.json())
+    .then((data) => addDataToDom(data));
+}
+ 
