7f33e31c0dcda1271cf85f7a0b82dc8fe2e87a63
Exxo05 <mrosarioperezcejuela@gmail.com>
2025-01-30 18:41:58 +0100

    añadi mas codigo de JS

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 let logotitulo = document.querySelector(".titulo");
 let elementoCuerpo = document.querySelector("cuerpo");
 
+window.addEventListener("load", () => {
+    let numeroRandom = Math.ceil(Math.random() * 5)
+    bodyElem.style.backgroundImage = `url(IMAGENES/bg${numeroRandom}.jpng)`;
+});
+
 
