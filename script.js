6d0d85c7ff865b12749bf859fe662ea6317e3856
Exxo05 <mrosarioperezcejuela@gmail.com>
2025-04-22 13:16:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ putCiudad.addEventListener("keypress", (event) => {
 
 let datosAPI = {
     url: "http://api.openweathermap.org/data/2.5/weather?q=",
-    key: "7ea15b5ede189da436e9374f884ca5b7",
+    key: "1234567890abcdefg",
     lang: "es"
 };
 
